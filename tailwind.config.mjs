7a65d0d16135f3c9affe10240f4fc17af89dd9b6
Pumang Kirange <kirangespumang03@gmail.com>
2024-05-05 14:48:03 +0530

    Home page without responsiveness

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 
-import defaultTheme from 'tailwindcss/defaultTheme'
+import defaultTheme from "tailwindcss/defaultTheme";
+
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-        sans: ['Open Sans Variable', ...defaultTheme.fontFamily.sans],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1899E9", // Define your custom color
+		
       },
-		},
-	},
-	plugins: [],
-}
+      fontFamily: {
+        sans: ["Open Sans Variable", ...defaultTheme.fontFamily.sans],
+        sora: ["Sora", "sans-serif"],
+      },
+	  scale: {
+        '175': '1.75',
+		'200': '2',
+      },
+    },
+  },
+  plugins: [],
+};
